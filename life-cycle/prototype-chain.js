4cab3fc41d598698f4355e72a56fe8e3aaf3ad3f
Lucas Coronel <lccoronel7@icloud.com>
2023-05-25 19:42:46 -0300

    wip: prototype-chain

---
+++ life-cycle/prototype-chain.js
@@ -0,0 +1,39 @@
+const assert = require('assert')
+
+const obj = {}
+const arr = []
+const fn  = () => {}
+
+// internamente, objetos literias viram  funçoes explicitas
+console.log('new Object() is {}', new Object().__proto__ === {}.__proto__)
+assert.deepStrictEqual(new Object().__proto__, {}.__proto__)
+
+// __proto__ é a referencia do objeto que possui as  propiedades nele
+console.log('obj.__proto__ === Object.prototype', obj.__proto__ === Object.prototype);
+assert.deepStrictEqual(obj.__proto__, Object.prototype)
+
+console.log('arr.__proto__ === Array.prototype', arr.__proto__ === Array.prototype);
+assert.deepStrictEqual(arr.__proto__, Array.prototype)
+
+console.log('fn.__proto__ === Function.prototype', fn.__proto__ === Function.prototype);
+assert.deepStrictEqual(fn.__proto__, Function.prototype)
+
+// o __proto__ de Object.prototype é null
+console.log('obj.__proto__.__proto__ === null', obj.__proto__.__proto__ === null);
+
+console.log('------------------------');
+
+function Employee() {}
+Employee.prototype.salary = () => "salary**"
+
+function Supervisor() {}
+// herda a instancia de Employee
+Supervisor.prototype = Object.create(Employee.prototype)
+Supervisor.prototype.profitShare = () => "profitShare**"
+
+function Manager() {}
+Manager.prototype = Object.create(Supervisor.prototype)
+Manager.prototype.monthlyBonuses = () => "monthlyBonuses**"
+
+// podemos chamar via prototype, mas se tentar chamar diretamente da erro
+console.log(Manager.prototype.salary())
