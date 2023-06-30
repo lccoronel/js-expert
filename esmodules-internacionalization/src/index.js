718646f07974ea9c5473bc168e85d3b49218bb19
Lucas Coronel <lccoronel7@icloud.com>
2023-06-30 07:55:28 -0300

    feat: terminal controller

--- esmodules-internacionalization/src/index.js
+++ esmodules-internacionalization/src/index.js
@@ -1,32 +1,29 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readline from 'readline'
-
 import database from './../database.json' assert { type: "json" }
+import Person from './persons.js'
+import TerminalController from './terminalController.js'
 
+const DEFAULT_LANG = 'pt-br'
+const STOP_TERM = ":q"
 
-DraftLog(console).addLineListener(process.stdin)
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
 
-const options = {
-    leftPad: 2,
-    columns: [
-        { field: "id", name: chalk.magenta("ID") },
-        { field: "vehicles", name: chalk.cyan("Vehicles") },
-        { field: "kmTraveled", name: chalk.cyan("KM Traveled") },
-        { field: "from", name: chalk.blue("From") },
-        { field: "to", name: chalk.blue("To") },
-    ]
+async function mainLoop() {
+    try {
+        const answer = await terminalController.question()
+        if (answer === STOP_TERM) {
+            terminalController.closeTerminal()
+            console.log('process finished!')
+            return;
+        }
+
+        const person = Person.generateInstanceFromString(answer)
+
+        return mainLoop()
+    } catch (error) {
+        console.error('DEU RUIM **', error);
+        return mainLoop()
+    }
 }
 
-const table = chalkTable(options, database)
-const print = console.draft(table)
-
-const terminal = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-terminal.question('Qual é o seu nome?', msg => {
-    console.log('msg', msg.toString());
-})
+await mainLoop()
